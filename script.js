55da0ac332830f0a11a3241099fa23309bc74510
ajartavastra <krishnans5324@gmail.com>
2026-02-18 21:08:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+async function loadProducts() {
+  const response = await fetch("data/products.json");
+  const products = await response.json();
+
+  let html = '<div class="grid">';
+
+  products.forEach((p) => {
+    const imagePath = `products/${p.Category}/${p.ProductCode} - ${p.ProductName}.jpg`;
+
+    html += `
+        <div class="card">
+            <img src="${imagePath}">
+            <h3>${p.Category}/${p.ProductCode} - ${p.ProductName}.jpg
+            <h3>${p.ProductName}</h3>
+            <p>${p.ProductCode}</p>
+            <p class="price">₹${formatPrice(p.Price)}</p>
+        </div>
+        `;
+  });
+
+  html += "</div>";
+
+  document.getElementById("content").innerHTML = html;
+}
+
+function formatPrice(price) {
+  return new Intl.NumberFormat("en-IN").format(price);
+}
+
+function loadGallery() {
+  document.getElementById("content").innerHTML =
+    "<h2 style='padding:20px'>Gallery will load from gallery folder.</h2>";
+}
