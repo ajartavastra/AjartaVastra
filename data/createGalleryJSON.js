fd882be326d1bbe9eddd634ff5be107ba9e6fe8c
ajartavastra <krishnans5324@gmail.com>
2026-03-16 14:43:51 +0530

    Add files via upload

--- data/createGalleryJSON.js
+++ data/createGalleryJSON.js
@@ -1,18 +1,19 @@
+/* to run -> node createGalleryJSON.js */
+
 const fs = require("fs");
 const path = require("path");
 
-const galleryDir = path.join(__dirname, "gallery");
+const galleryDir = path.join(__dirname, "../gallery");
 
 let result = {};
 
 fs.readdirSync(galleryDir).forEach((folder) => {
   const folderPath = path.join(galleryDir, folder);
-
   if (fs.statSync(folderPath).isDirectory()) {
     result[folder] = fs
       .readdirSync(folderPath)
       .filter((file) => file.toLowerCase().endsWith(".jpg"))
-      .map((file) => folder + "/" + file);
+      .map((file) => "gallery/" + folder + "/" + file);
   }
 });
 
